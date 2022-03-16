1a55435349cf068151031895cc103320381dba4d
Couqdewit <nquocn2402@gmail.com>
2022-03-16 20:54:09 +0700

    AG32: Details - Create and design  div contain img

--- client/src/pages/Details/index.js
+++ client/src/pages/Details/index.js
@@ -1,7 +1,50 @@
+import { Link } from "react-router-dom";
+
 export default function Details() {
-    return (
-        <div>
-            Details
+  return (
+    <section>
+      <div className="container bg-quaternary-500">
+        <div className=" text-center min-h-[720px] bg-hero-pattern-details bg-cover bg-center bg-fixed bg-no-repeat flex-center flex-col gap-y-4"></div>
+
+        <div className="relative h-[1200px] w-[100%]">
+          <div className="bg-capuchino h-[700px] w-[100%] bg-contain bg-center bg-no-repeat absolute inset-y-28 left-0 w-[50%] ...">
+            <div className="bg-vec-left bg-left"></div>
+            <div className="bg-vec-right bg-right"></div>
+
+
+            <div className="bg-capuchino bg-contain bg-center bg-no-repeat absolute h-[90px] w-[90px] top-[760px] left-[240px] w-[100%] ..."></div>
+            <div className="bg-capuchino bg-contain bg-center bg-no-repeat absolute h-[90px] w-[90px] top-[760px] left-[340px] w-[100%] ..."></div>
+            <div className="bg-capuchino bg-contain bg-center bg-no-repeat absolute h-[90px] w-[90px] top-[760px] left-[440px] w-[100%] ..."></div>
+          </div>
+
+          <div className="info text-center float-right bg-center relative inset-y-28 left-0 w-[50%] ...">
+            <h1>Capuchino</h1>
+            <p>sdasdsa</p>
+            <p>sdasdsa</p>
+            <p>sdasdsa</p>
+            <p>sdasdsa</p>
+            <p>sdasdsa</p>
+            <p>sdasdsa</p>  
+            <p>sdasdsa</p>
+            <p>sdasdsa</p>
+            <p>sdasdsa</p>
+            <p>sdasdsa</p>
+            <p>sdasdsa</p>
+            <p>sdasdsa</p>
+            <p>sdasdsa</p>
+            <p>sdasdsa</p>
+            <p>sdasdsa</p>
+            <p>sdasdsa</p>
+            <p>sdasdsa</p>
+            <p>sdasdsa</p>
+            <p>sdasdsa</p>
+            <p>sdasdsa</p>
+            <p>sdasdsa</p>
+            <p>sdasdsa</p>
+            <p>sdasdsa</p>
+          </div>
         </div>
-    );
+      </div>
+    </section>
+  );
 }
