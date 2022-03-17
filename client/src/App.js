c76a4dcdbea0fb743ba25471b9a77d4ba73dc25c
Couqdewit <nquocn2402@gmail.com>
2022-03-17 20:34:45 +0700

    AG32: Details - New Pull Project

--- client/src/App.js
+++ client/src/App.js
@@ -4,40 +4,10 @@ import Details from "pages/Detail";
 import Drinks from "pages/Drinks";
 import Example from "pages/Example";
 import Home from "pages/Home";
-import Details from "pages/Details";
+// import Details from "pages/Details";
 import { useRoutes } from "react-router-dom";
 
 export default function App() {
-<<<<<<< HEAD
-	return useRoutes([
-		{
-			path: "/",
-			element: <PublicLayout />,
-			children: [
-				{
-					index: true,
-					element: <Home />,
-				},
-				{
-					path: "cart",
-					element: <Cart />,
-				},
-				{
-					path: 'drinks',
-					element: <Drinks />,
-				},
-				{
-					path: 'details',
-					element: <Details />,
-				},
-				{
-					path: 'example',
-					element: <Example />,
-				},
-			]
-		},
-	]);
-=======
     return useRoutes([
         {
             path: "/",
@@ -71,5 +41,4 @@ export default function App() {
             ],
         },
     ]);
->>>>>>> 06778462eb41b928df74e9682eb4879e52906a34
 }
